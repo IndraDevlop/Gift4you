bfa20e6e70f7f03cf3c714fafd97d37f6b2e8d04
InDev <72058020+IndraDevlop@users.noreply.github.com>
2023-09-01 17:39:34 +0700

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-onload = () =>{
-        document.body.classList.remove("container");
-        var musik = "musik.mp3";
-        var audio = document.querySelector(".audio");
-        audio.src = musik; 
-        audio.play();
-};
